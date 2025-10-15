ab8f5c2d890915506891e5b2a70a576bcf4f41e1
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2025-10-15 17:22:28 +0800

    simple html file default popup

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-chrome.action.onClicked.addListener(tab => {
-    // execute script in the context of the current tab
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: () => {
-            alert('Hello from the extension!');
-        }
-    })
-})
